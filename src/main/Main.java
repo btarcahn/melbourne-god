b74a659ff5489a03ae3ea231ce95941d88196eab
Bach <batr.schip@gmail.com>
2019-01-21 14:43:02 -0500

    initialize the project

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("G'day folks, I'm from Melbourne, Australia.");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Bring me Thanossss!!!");
+		}
+	}
+}
